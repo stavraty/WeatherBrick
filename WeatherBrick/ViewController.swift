9792278da732f743bced54eab76661104584ce0e
Stavratiy Andriy <stavraty@gmail.com>
2023-03-14 08:12:22 +0200

    1

--- WeatherBrick/ViewController.swift
+++ WeatherBrick/ViewController.swift
@@ -9,5 +9,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //
     }
 }
