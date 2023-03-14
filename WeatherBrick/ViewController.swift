7d9b3f7b98d369b2e49fb76dd59b3e04cfb65939
Stavratiy Andriy <stavraty@gmail.com>
2023-03-14 07:33:33 +0200

    Initial Commit

---
+++ WeatherBrick/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Volodymyr Andriienko on 11/3/21.
+//  Copyright © 2021 VAndrJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
